baa6aeb334cee9b4c16a104901ae83600ec2c458
maisyhallam <m.hallam@sms.ed.ac.uk>
2023-03-02 16:27:10 +0000

    updated ics script and proportion symmetry script

--- code/ICS_franken.py
+++ code/ICS_franken.py
@@ -3,57 +3,35 @@ import csv
 import os
 from collections import Counter
 
-# pairs is a dictionary showing which kinbank shorthands represent which family relationships
-# each tuple in pairs.values represents the pair of kin terms that will be compared for equality
+# list of relevant relationships in each gen (index of both lists gives a parent-child pair)
 gen2 = ['mMB','mMeB','mMyB','mMZ','mMeZ','mMyZ','mFB','mFeB','mFyB','mFZ','mFeZ','mFyZ']
-gen1 = ['mMBS', 'mMBeS', 'mMByS','mMZS','mMZeS','mMZyS','mFBS','mFBeS','mFByS','mFZS','mFZeS','mFByS'] # just one child to keep things simple
+gen1 = ['mMBS', 'mMeBS', 'mMyBS','mMZS','mMeZS','mMyZS','mFBS','mFeBS','mFyBS','mFZS','mFeZS','mFyBS'] # just one child to keep things simple
 
-# {'siblings': [('meB','meZ'),('myB','myZ')],
-# 'm_siblings': [('mMeB','mMeZ'),('mMyB','mMyZ')],
-# 'f_siblings': [('mFeB','mFeZ'),('mFyB','mFyZ')],
-# 'mez_cousins': [('mMeZeS','mMeZeD'),('mMeZyS','mMeZyD'),('mMeZS','mMeZD')],
-# 'meb_cousins': [('mMeBeS','mMeBeD'),('mMeByS','mMeByD'),('mMeBS','mMeBD')],
-# 'fez_cousins': [('mFeZeS','mFeZeD'),('mFeZyS','mFeZyD'),('mFeZS','mFeZD')],
-# 'feb_cousins': [('mFeBeS','mFeBeD'),('mFeByS','mFeByD'),('mFeBS','mFeBD')],
-# 'myz_cousins': [('mMyZeS','mMyZeD'),('mMyZyS','mMyZyD'),('mMyZS','mMyZD')],
-# 'myb_cousins': [('mMyBeS','mMyBeD'),('mMyByS','mMyByD'),('mMyBS','mMyBD')],
-# 'fyz_cousins': [('mFyZeS','mFyZeD'),('mFyZyS','mFyZyD'),('mFyZS','mFyZD')],
-# 'fyb_cousins': [('mFyBeS','mFyBeD'),('mFyByS','mFyByD'),('mFyBS','mFyBD')],
-# 'mz_cousins': [('mMZeS','mMZeD'),('mMZyS','mMZyD'),('mMZS','mMZD')],
-# 'mb_cousins': [('mMBeS','mMBeD'),('mMByS','mMByD'),('mMBS','mMBD')],
-# 'fz_cousins': [('mFZeS','mFZeD'),('mFZyS','mFZyD'),('mFZS','mFZD')],
-# 'fb_cousins': [('mFBeS','mFBeD'),('mFByS','mFByD'),('mFBS','mFBD')]
-# }
+# list of language families, ie directory names that the kinbank data is divided into
+families = ['Austroasiatic', 'Sino-Tibetan', 'Afro-Asiatic',
+'Indo-European', 'Pano-Tacanan', 'Nuclear Trans New Guinea',
+'Pama-Nyungan', 'Tai-Kadai', 'Other', 'Arawakan', 'Austronesian', 'Algic',
+'Cariban', 'Nakh-Daghestanian', 'Turkic', 'Dravidian', 'Uto-Aztecan',
+'Atlantic-Congo', 'Uralic', 'Tupian']
 
-def get_kin(file,directory):
-    """Loads in a frankenlanguage csv file, populates a dictionary with the kin terms."""
-    # file_path = '../frankenlanguages/' + directory + '/' + file
-    file_path = '../kinbank/raw/' + directory + '/' + file
+def get_kin(file_path,type,term):
+    """Loads in a language csv file, populates a dictionary with the kin terms."""
     kin_terms = {}
     with open(file_path, encoding="utf8") as f:
         csv_reader = csv.DictReader(f)
         for line in csv_reader:
-            parameter = line['parameter'] # kin type
-            word = line['word'] # kin term
+            parameter = line[type] # kin type
+            word = line[term] # kin term
             kin_terms[parameter] = word
     return kin_terms
 
-# def find_relevant_kin(generation):
-#     relevant_kin = []
-#     for kin_types in generation:
-#         # for j in i:
-#         #     index = generation2.index(j)
-#         #     if j == 'na'
-#         #     generation2.remove(j)
-#         #     generation1.remove(generation1[i][j])
-#         if len(set(kin_types)) == 1:
-#             relevant_kin.append([x for x in kin_types if x == kin_types[0]])
-#         elif len(set(kin_types)) == 2:
-#             relevant_kin.append(list(dict.fromkeys(kin_types)))
-#         else:
-#             continue
-#
-#     return relevant_kin
+def get_csv_files(path):
+    """Returns a list of csv filenames."""
+    directory = os.scandir(path)
+    files = []
+    for file in directory:
+        files.append(file.name)
+    return files
 
 def list_generations(ks,gen):
     """Returns a list of the kin terms for each kin type in gen, na if not available in the data."""
@@ -65,29 +43,9 @@ def list_generations(ks,gen):
         else:
             placeholder.append('na')
         generation += placeholder
-    # generation = list(generation[i:i+3] for i in range(0,len(list(generation)),3)) # because there are 3 terms for each kin type right now
-    # relevant_kin = find_relevant_kin(generation)
-    # print(relevant_kin)
-    print(generation)
     return generation
 
 
-
-
-# def find_duplicates(gen):
-#     seen = set()
-#     dupes = []
-#     print(gen)
-#
-#     for term in gen.values():
-#         if term in seen:
-#             dupes.append(term)
-#         else:
-#             seen.add(term)
-#     return dupes
-
-def flatten(l):
-    return [item for sublist in l for item in sublist]
 
 def code_kin_types(gen):
     codes = {}
@@ -112,110 +70,148 @@ def enumerate_generation(gen):
     codes = code_kin_types(gen)
     code = 0
 
-    for kin_type in set(gen):
+    gen_pattern = [i for i in gen]
 
-        while kin_type in gen:
-            index = gen.index(kin_type)
 
-            # label it 0 if na
-            if kin_type == 'na':
-                gen[index] = 0
+    for kin_type in set(gen_pattern):
+
+        while kin_type in gen_pattern:
+            index = gen_pattern.index(kin_type)
+
+            # label it 0 if na, or otherwise unspecified in the original data
+            if kin_type == 'na' or kin_type == '0' or kin_type == "":
+                gen_pattern[index] = 0
             # give it a unique number if anything else
             else:
-                gen[index] = codes[kin_type]
+                gen_pattern[index] = codes[kin_type]
 
-    # gen_pattern = list(gen_pattern[i:i+length] for i in range(0,len(list(gen)),length))
-    print(gen)
-    return gen
+    return gen_pattern
 
-# def enumerate_generation1(gen):
-#     """Turn lists of Gen1 kin terms into lists of numbers. Each number indicates a unique kin term."""
-#     gen = list(gen.values())
-#     gen_pattern = [1,2,3,4] # placeholder list to maintain index range
-#     code = 0
-#
-#     for item in set(gen): # for each unique item in gen
-#         code += 1
-#         index = gen.index(item)
-#         index += 1
-#         if (index % 2) == 0: # check if index + 1 is even
-#             continue # do nothing if so
-#         else:
-#             while item in gen:
-#                 index = gen.index(item)
-#                 if item == 'na':
-#                     gen_pattern[index] = 0
-#                 else:
-#                     gen_pattern[index] = code
-#                 gen.remove(item)
-#     return gen_pattern
-#
-#
-#     gen_pattern = [list(gen.values())[i:i+2] for i in range(0,len(list(gen.values())),2)]
-#
-#     print(gen_pattern)
-#     return gen_pattern
-
-
-def check_pattern(pattern1,pattern2):
+def ICS_score(pattern1,pattern2):
     """Checks whether the categories of kin are the same in gen2 as gen1."""
     ICS_score = 0
-    denominator = 4
+    denominator = len(gen1)
 
     # for each type of kin
-    for i in range(4):
-        if pattern2[i] == pattern1[2]: # order of the list matters: so same index of both lists will be the parent/child pair
-            if pattern1[i] == 0 or pattern2[i] == 0:
-                continue
-            else:
-                ICS_score += 1
+    for i in range(len(gen1)):
+        if pattern1[i] == 0 or pattern2[i] == 0:
+            denominator -= 1
+        elif pattern1[i] == pattern2[i]:
+            ICS_score += 1
         else:
-            continue
-    print(ICS_score/denominator)
+            pass
 
-def write_full_csv(filename,language,generation_results):
-    """Creates a list of a languages two generation scores, and writes the language name + score to a csv file if the score is not 'null'."""
+    all_data = pattern1 + pattern2
 
-    result = []
-    for i in generation_results:
-        result.append(generation_results[i])
+    # # if all of one generation is identical, automatic co-selection
+    # if len(set(pattern1)) == 1 or len(set(pattern2)) == 1:
+    #     ICS_score = denominator
 
-
-    if 'null' in result:
+    # if there is no data for one of the generations, exclude this language
+    if sum(pattern1) == 0 or sum(pattern2) == 0:
+        pass
+    # if there is no data for any of the relevant relationships, exclude this language
+    elif denominator == 0:
+        pass
+    # if more than half the data is missing, exclude this language
+    elif all_data.count(0) > 12:
         pass
     else:
-        with open(filename, 'a', encoding='utf8') as csv_file:
-            csvwriter = csv.writer(csv_file)
-            csvwriter.writerow([language,result])
+        return (ICS_score/denominator)
 
 def write_headers(filename):
-    with open(filename, 'a', encoding='utf8') as csv_file:
+    file_path = '../data/ICS/' + filename + '.csv'
+    with open(file_path, 'a', encoding='utf8') as csv_file:
         csvwriter = csv.writer(csv_file)
-        csvwriter.writerow(['LANGUAGE','GENDER'])
+        csvwriter.writerow(['LANGUAGE','SCORE'] + gen1 + gen2)
 
-def main(argv):
-    directory_counter = 0
+def write_full_csv(filename,language,gen1,gen2,score):
+    """Writes the language, proportion of ICS, and each kin type's kin term to a csv file"""
+    file_path = '../data/ICS/' + filename + '.csv'
+    row = [language] + [score] + gen1 + gen2
 
-    for i in range(int(argv)):
-        directory_counter += 1
-        files = get_csv_files('../frankenlanguages/' + str(directory_counter))
-        write_headers('../data/frankenlanguages_ICS.csv')
+    with open(file_path, 'a', encoding='utf8') as csv_file:
+        csvwriter = csv.writer(csv_file)
+        csvwriter.writerow(row)
 
-        for file in files:
-            kin_terms = get_kin(file,str(directory_counter))
-            feature_symmetry_results = check_feature_symmetry(kin_terms)
-            gen_results = generation_results(feature_symmetry_results)
+def get_language_name(string):
+    """Extract the language name from the filename of kinbank data."""
+    language_name = ''
+    if string.startswith('Morgan1871_'):
+        string = string[len('Morgan1871_'):]
+    for i in range(len(string)-13):
+        language_name += string[i]
+    return language_name
 
-            write_full_csv('../data/frankenlanguages_ICS.csv',file + '_' + str(directory_counter),gen_results)
 
-# if __name__ == '__main__':
-#     main(sys.argv[1])
+def main(data_type,count):
+    """Data_type is kinbank or frankenlanguage,count is the number of iterations (1000 for franken)."""
+    if data_type == 'kinbank':
+        write_headers(data_type)
+        for family in families:
+            files = get_csv_files('../kinbank/raw/' + family)
 
-ks = get_kin('English_stan1293.csv','Indo-European')
-generation1 = list_generations(ks,gen1)
-generation2 = list_generations(ks,gen2)
-gen1_pattern = enumerate_generation(generation1)
-gen2_pattern = enumerate_generation(generation2)
+            for file in files:
+                ks = get_kin('../kinbank/raw/' + family + '/' + file,'parameter','word')
+                language = get_language_name(file)
+                generation1 = list_generations(ks,gen1)
+                generation2 = list_generations(ks,gen2)
+                gen1_pattern = enumerate_generation(generation1)
+                gen2_pattern = enumerate_generation(generation2)
+                score = ICS_score(gen1_pattern,gen2_pattern)
+                if score:
+                    write_full_csv(data_type,language,generation1,generation2,score)
+                else:
+                    pass
+    if data_type == 'frankenlanguage':
+        directory_counter = 0
+        for i in range(int(count)):
+            directory_counter += 1
+            files = get_csv_files('../frankenlanguages/' + str(directory_counter))
+            write_headers(data_type + '_' + str(directory_counter))
 
-# print(gen1_pattern,gen2_pattern)
-check_pattern(gen1_pattern,gen2_pattern)
+            for file in files:
+                ks = get_kin('../frankenlanguages/' + str(directory_counter) + '/' + file,'TYPE','TERM')
+                language = file + '_' + str(directory_counter)
+                generation1 = list_generations(ks,gen1)
+                generation2 = list_generations(ks,gen2)
+                gen1_pattern = enumerate_generation(generation1)
+                gen2_pattern = enumerate_generation(generation2)
+                score = ICS_score(gen1_pattern,gen2_pattern)
+                if score:
+                    write_full_csv(data_type + '_' + str(directory_counter),language,generation1,generation2,score)
+                else:
+                    pass
+
+
+
+
+    # directory_counter = 0
+    #
+    # for i in range(int(argv)):
+    #     directory_counter += 1
+    #     files = get_csv_files('../frankenlanguages/' + str(directory_counter))
+    #     write_headers('../data/frankenlanguages_ICS.csv')
+    #
+    #     for file in files:
+    #         kin_terms = get_kin(file,str(directory_counter))
+    #         feature_symmetry_results = check_feature_symmetry(kin_terms)
+    #         gen_results = generation_results(feature_symmetry_results)
+    #
+    #         write_full_csv('../data/frankenlanguages_ICS.csv',file + '_' + str(directory_counter),gen_results)
+
+if __name__ == '__main__':
+    main(sys.argv[1],sys.argv[2])
+
+# ks = get_kin('English_stan1293.csv','Indo-European')
+# generation1 = list_generations(ks,gen1)
+# generation2 = list_generations(ks,gen2)
+# print(generation1)
+# gen1_pattern = enumerate_generation(generation1)
+# gen2_pattern = enumerate_generation(generation2)
+# print(generation1)
+#
+#
+# # print(gen1_pattern,gen2_pattern)
+# score = ICS_score(gen1_pattern,gen2_pattern)
+# write_full_csv('English','English',generation1,generation2,score)
--- code/proportion_gender_symmetry_franken.py
+++ code/proportion_gender_symmetry_franken.py
@@ -23,48 +23,64 @@ gender_pairs = {
                ('mFBeS','mFBeD'),('mFByS','mFByD'),('mFBS','mFBD')]
 }
 
+generation1_pairs = [('meB','meZ'),('myB','myZ'),('mMeZeS','mMeZeD'),('mMeZyS','mMeZyD'),('mMeZS','mMeZD'),
+('mMyZeS','mMyZeD'),('mMyZyS','mMyZyD'),('mMyZS','mMyZD'),('mMZeS','mMZeD'),('mMZyS','mMZyD'),('mMZS','mMZD'),
+('mMeBeS','mMeBeD'),('mMeByS','mMeByD'),('mMeBS','mMeBD'),('mMyBeS','mMyBeD'),('mMyByS','mMyByD'),('mMyBS','mMyBD'),
+('mMBeS','mMBeD'),('mMByS','mMByD'),('mMBS','mMBD'),('mFeZeS','mFeZeD'),('mFeZyS','mFeZyD'),('mFeZS','mFeZD'),
+('mFyZeS','mFyZeD'),('mFyZyS','mFyZyD'),('mFyZS','mFyZD'),('mFZeS','mFZeD'),('mFZyS','mFZyD'),('mFZS','mFZD'),
+('mFeBeS','mFeBeD'),('mFeByS','mFeByD'),('mFeBS','mFeBD'),('mFyBeS','mFyBeD'),('mFyByS','mFyByD'),('mFyBS','mFyBD'),
+('mFBeS','mFBeD'),('mFByS','mFByD'),('mFBS','mFBD')]
+
+generation2_pairs = [('mMeB','mMeZ'),('mMyB','mMyZ'),('mFeB','mFeZ'),('mFyB','mFyZ')]
+
 kin_types = ['siblings','m_siblings','f_siblings','mz_cousins','mb_cousins','fz_cousins','fb_cousins']
 
 generation1 = ['siblings', 'mz_cousins','mb_cousins','fz_cousins','fb_cousins']
 generation2 = ['m_siblings','f_siblings']
 
-def get_kin(file,directory):
+# list of language families, ie directory names that the kinbank data is divided into
+families = ['Austroasiatic', 'Sino-Tibetan', 'Afro-Asiatic',
+'Indo-European', 'Pano-Tacanan', 'Nuclear Trans New Guinea',
+'Pama-Nyungan', 'Tai-Kadai', 'Other', 'Arawakan', 'Austronesian', 'Algic',
+'Cariban', 'Nakh-Daghestanian', 'Turkic', 'Dravidian', 'Uto-Aztecan',
+'Atlantic-Congo', 'Uralic', 'Tupian']
+
+def get_kin(file_path,type,term):
     """Loads in a frankenlanguage csv file, populates a dictionary with the kin terms."""
-    file_path = '../frankenlanguages/' + directory + '/' + file
     kin_terms = {}
     with open(file_path, encoding="utf8") as f:
         csv_reader = csv.DictReader(f)
         for line in csv_reader:
-            parameter = line['TYPE'] # kin type
-            word = line['TERM'] # kin term
+            parameter = line[type] # kin type
+            word = line[term] # kin term
             kin_terms[parameter] = word
     return kin_terms
 
-# def denominator(generation_list):
-#     """Returns the number of possible relationships that could have this distinction."""
-#     denominator = 0
-#     for kin_type in generation_list:
-#         for pair in gender_pairs[kin_type]:
-#             denominator += 1
-#     return denominator
+def denominator(generation_list,number):
+    """Returns the number of possible relationships that could have this distinction."""
+    denominator = 0
+    for kin_type in generation_list:
+        for pair in gender_pairs[kin_type]:
+            denominator += 1
+    denominator -= number
+    return denominator
 
 def check_feature_symmetry(ks):
     """Checks whether the terms used in the language for each pair of kin terms match."""
     results = {}
 
     for kin in kin_types:
-        results[kin] = 0
+        count = 0
         for pair in gender_pairs[kin]:
             if pair[0] in ks.keys() and pair[1] in ks.keys():
                 if ks[pair[0]] == ks[pair[1]]:
-                    continue
+                    results[pair] = 0
                 else:
-                    results[kin] = 1
+                    results[pair] = 1
             else:
-                continue
-        if kin not in results:
-            results[kin] = 'null'
+                results[pair] = 'null'
 
+    print(results)
     return results
 
 def get_csv_files(path):
@@ -74,6 +90,16 @@ def get_csv_files(path):
     for file in directory:
         files.append(file.name)
     return files
+
+def get_language_name(string):
+    """Extract the language name from the filename of kinbank data."""
+    language_name = ''
+    if string.startswith('Morgan1871_'):
+        string = string[len('Morgan1871_'):]
+    for i in range(len(string)-13):
+        language_name += string[i]
+    return language_name
+
 
 def write_symmetry_csv(filename,file,symmetries):
     """Writes a csv file with the name of each frankenlanguage file plus its symmetry score."""
@@ -86,26 +112,32 @@ def generation_results(feature_symmetry_results):
     results = {}
     gen1_values = []
     gen2_values = []
+
     for i in feature_symmetry_results:
-        if i in generation1:
+        if i in generation1_pairs:
             gen1_values.append(feature_symmetry_results[i])
         else:
             gen2_values.append(feature_symmetry_results[i])
 
-# unsure whether to keep this or not - seems unfair either way to count or not
-# count where there isn't enough data to know for sure if a distinction is made
-    for j in gen1_values:
-        if j == 'null':
-            gen1_values.remove(j)
-            gen1_denominator -= 1
+    gen1_nulls = gen1_values.count('null')
+    gen2_nulls = gen2_values.count('null')
 
-    for k in gen2_values:
-        if k == 'null':
-            gen2_values.remove(k)
-            gen2_denominator -= 1
+    # unsure whether to keep this or not - seems unfair either way to count or not count where there isn't enough data to know for sure if a distinction is made
+    gen1_values = [x for x in gen1_values if x != 'null']
+    gen2_values = [x for x in gen2_values if x != 'null']
 
-    results['generation1'] = sum(gen1_values)/len(generation1)
-    results['generation2'] = sum(gen2_values)/len(generation2)
+    gen1_denominator = denominator(generation1,gen1_nulls)
+    gen2_denominator = denominator(generation2,gen2_nulls)
+
+    if gen1_denominator == 0:
+        results['generation1'] = 'null'
+    else:
+        results['generation1'] = sum(gen1_values)/gen1_denominator
+
+    if gen2_denominator == 0:
+        results['generation2'] = 'null'
+    else:
+        results['generation2'] = sum(gen2_values)/gen2_denominator
 
     return results
 
@@ -115,31 +147,52 @@ def write_full_csv(filename,language,generation_results,set):
     """Creates a list of a languages two generation scores, and writes the language name + score to a csv file if the score is not 'null'."""
     generation1 = generation_results['generation1']
     generation2 = generation_results['generation2']
-    with open(filename, 'a', encoding='utf8') as csv_file:
-        csvwriter = csv.writer(csv_file)
-        csvwriter.writerow([language,generation1,generation2,set])
+    if generation1 == 'null' or generation2 == 'null':
+        pass
+    else:
+        with open(filename, 'a', encoding='utf8') as csv_file:
+            csvwriter = csv.writer(csv_file)
+            csvwriter.writerow([language,generation1,generation2,set])
 
 def write_headers(filename):
     with open(filename, 'a', encoding='utf8') as csv_file:
         csvwriter = csv.writer(csv_file)
         csvwriter.writerow(['LANGUAGE','GEN_0','GEN_1','SET'])
 
-def main(argv):
-    directory_counter = 0
-
-    for i in range(int(argv)):
-        directory_counter += 1
-        filename = '../data/gender/prop/prop_set' + str(directory_counter) + '.csv'
-        files = get_csv_files('../frankenlanguages/' + str(directory_counter))
+def main(data_type,count):
+    if data_type == 'kinbank':
+        filename = '../data/gender/prop/kinbank.csv'
         write_headers(filename)
 
-        for file in files:
-            kin_terms = get_kin(file,str(directory_counter))
-            feature_symmetry_results = check_feature_symmetry(kin_terms)
-            gen_results = generation_results(feature_symmetry_results)
-            language = file + '_' + str(directory_counter)
+        for family in families:
+            files = get_csv_files('../kinbank/raw/' + family)
 
-            write_full_csv(filename,language,gen_results,str(directory_counter))
+            for file in files:
+                ks = get_kin('../kinbank/raw/' + family + '/' + file,'parameter','word')
+                language = get_language_name(file)
+                feature_symmetry_results = check_feature_symmetry(ks)
+                gen_results = generation_results(feature_symmetry_results)
+
+                write_full_csv(filename,language,gen_results,'kinbank')
+
+    if data_type == 'frankenlanguage':
+        file_path = '../frankenlanguages/'
+        directory_counter = 0
+
+        for i in range(int(count)):
+            directory_counter += 1
+            filename = '../data/gender/prop/prop_set' + str(directory_counter) + '.csv'
+            files = get_csv_files(file_path + str(directory_counter))
+            write_headers(filename)
+
+
+            for file in files:
+                ks = get_kin(file_path + str(directory_counter) + '/' + file,'TYPE','TERM')
+                feature_symmetry_results = check_feature_symmetry(ks)
+                gen_results = generation_results(feature_symmetry_results)
+                language = file + '_' + str(directory_counter)
+
+                write_full_csv(filename,language,gen_results,str(directory_counter))
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1],sys.argv[2])
